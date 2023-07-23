fcc14d2b296ddb1c255279551b8d73744fdd03c7
Sean <seandavis587@gmail.com>
2023-07-24 03:01:19 +0800

    Function file seperated

---
+++ Functions.py
@@ -0,0 +1,102 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QMessageBox
+from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
+from PyQt5 import QtWidgets, QtGui, QtCore
+from BDPS_ui import Ui_MainWindow
+from BDPS_dbconnect.BDPS_queries import DBConnect
+
+
+class BtnFunctions(QMainWindow):
+    def __init__(self):
+        super(BtnFunctions, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.icon_only_widget.hide()
+        self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.home_btn_2.setChecked(True)
+    
+        self.ui.search_pricelist_btn.clicked.connect(self.pricelist_clicked)
+        self.ui.search_category_btn.clicked.connect(self.category_clicked)
+    
+    # Price list search button   
+    def pricelist_clicked(self):
+        print("pricelist")
+    
+    # Category search button 
+    def category_clicked(self):
+        print("category")    
+        
+    ## Function for searching
+    def on_search_btn_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(5)
+        search_text = self.ui.search_input.text().strip()
+        if search_text:
+            self.ui.label_9.setText(search_text)
+
+    ## Function for changing page to user page
+    def on_user_btn_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(6)
+
+    ## Change QPushButton Checkable status when stackedWidget index changed
+    def on_stackedWidget_currentChanged(self, index):
+        btn_list = self.ui.icon_only_widget.findChildren(QPushButton) \
+                    + self.ui.full_menu_widget.findChildren(QPushButton)
+        
+        for btn in btn_list:
+            if index in [7, 8]:
+                btn.setAutoExclusive(False)
+                btn.setChecked(False)
+            else:
+                btn.setAutoExclusive(True)
+            
+    ## ----------------------- functions for changing menu page ---------------------
+    def on_home_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def on_home_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def on_price_list_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+
+    def on_price_list_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+
+    def on_categories_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+    def on_categories_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+    def on_new_transaction_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+    def on_new_transaction_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+    def on_transaction_record_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(4)
+
+    def on_transaction_record_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(4)
+
+    def on_daily_transaction_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(5)
+
+    def on_daily_transaction_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(5)
+
+    def on_daily_transaction_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(6)
+
+    def on_datewise_transaction_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(6)
+    
+    def on_datewise_payment_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(7)
+
+    def on_daily_payment_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(7)
+
+     ## ----------------------- ADD SHADOW --------------------- 
--- main.py
+++ main.py
@@ -1,117 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QMessageBox
 from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
-from PyQt5 import QtWidgets, QtGui, QtCore
-from BDPS_ui import Ui_MainWindow
-from BDPS_dbconnect.BDPS_queries import DBConnect
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-        self.ui.icon_only_widget.hide()
-        self.ui.stackedWidget.setCurrentIndex(0)
-        self.ui.home_btn_2.setChecked(True)
-        
-        self.ui.search_pricelist_btn.clicked.connect(self.show_info_messagebox)
-
-        
-    ## Function for searching
-    def on_search_btn_clicked(self):
-        self.ui.stackedWidget.setCurrentIndex(5)
-        search_text = self.ui.search_input.text().strip()
-        if search_text:
-            self.ui.label_9.setText(search_text)
-
-    ## Function for changing page to user page
-    def on_user_btn_clicked(self):
-        self.ui.stackedWidget.setCurrentIndex(6)
-
-    ## Change QPushButton Checkable status when stackedWidget index changed
-    def on_stackedWidget_currentChanged(self, index):
-        btn_list = self.ui.icon_only_widget.findChildren(QPushButton) \
-                    + self.ui.full_menu_widget.findChildren(QPushButton)
-        
-        for btn in btn_list:
-            if index in [7, 8]:
-                btn.setAutoExclusive(False)
-                btn.setChecked(False)
-            else:
-                btn.setAutoExclusive(True)
-            
-    ## ----------------------- functions for changing menu page ---------------------
-    def on_home_btn_1_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(0)
-
-    def on_home_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(0)
-
-    def on_price_list_btn_1_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(1)
-
-    def on_price_list_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(1)
-
-    def on_categories_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(2)
-
-    def on_categories_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(2)
-
-    def on_new_transaction_btn_1_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(3)
-
-    def on_new_transaction_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(3)
-
-    def on_transaction_record_btn_1_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(4)
-
-    def on_transaction_record_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(4)
-
-    def on_daily_transaction_btn_1_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(5)
-
-    def on_daily_transaction_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(5)
-
-    def on_daily_transaction_btn_1_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(6)
-
-    def on_datewise_transaction_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(6)
-    
-    def on_datewise_payment_btn_1_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(7)
-
-    def on_daily_payment_btn_2_toggled(self):
-        self.ui.stackedWidget.setCurrentIndex(7)
- 
-    ## ---------------- Trial for search button in pricelist ------------------
-    
-    def on_search_pricelist_clicked(self):
-        self.ui.label_10.setText("Button Clicked!")
-    
-    def show_info_messagebox(self):
-        self.msg = QMessageBox()
-        self.msg.setIcon(QMessageBox.Information)
-    
-        # setting message for Message Box
-        self.msg.setText("Information ")
-        
-        # setting Message box window title
-        self.msg.setWindowTitle("Information MessageBox")
-        
-        # declaring buttons on Message Box
-        self.msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        
-        retval = self.msg.exec_()  
-
-     ## ----------------------- ADD SHADOW --------------------- 
+from Functions import BtnFunctions
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -126,8 +16,8 @@ if __name__ == "__main__":
     style_stream = QTextStream(style_file)
     app.setStyleSheet(style_stream.readAll())
 
-    window = MainWindow()
-    window.show()
+    Functions = BtnFunctions()
+    Functions.show()
 
     sys.exit(app.exec())
 
