6d8ec5318b8f647e4d850c94df916adb3e7d377b
abldnbnr <donobanrabella04@gmail.com>
2023-08-02 18:07:47 +0800

    More pop up

--- POP_UP/7Inv_id.py
+++ POP_UP/7Inv_id.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '0-Success.ui'
+# Form implementation generated from reading ui file '7Inv_id.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -13,22 +13,33 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Dialog(object):
 
-
+    
     def press_ok(self, Dialog):
         Dialog.close()
 
 
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 420)
+        Dialog.resize(400, 352)
+
+
+        self.okay_btn = QtWidgets.QPushButton(Dialog, clicked=lambda: self.press_ok(Dialog))
+        self.okay_btn.setGeometry(QtCore.QRect(30, 280, 341, 41))
+
+
+        font = QtGui.QFont()
+        font.setFamily("SF Pro Display")
+        font.setPointSize(9)
+        self.okay_btn.setFont(font)
+        self.okay_btn.setObjectName("okay_btn")
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(100, 40, 200, 200))
+        self.label.setGeometry(QtCore.QRect(150, 30, 100, 100))
         self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("check.png"))
+        self.label.setPixmap(QtGui.QPixmap("[SUB] Information Management/Transaction-Management-System-main/Transaction-Management-System/POP_UP/1-Categories/delete_warning.png"))
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(0, 230, 401, 71))
+        self.label_2.setGeometry(QtCore.QRect(10, 120, 391, 71))
         font = QtGui.QFont()
         font.setFamily("SF Pro Display")
         font.setPointSize(16)
@@ -36,21 +47,15 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-
-
-        self.pushButton = QtWidgets.QPushButton(Dialog, clicked=lambda: self.press_ok(Dialog))
-        self.pushButton.setGeometry(QtCore.QRect(50, 320, 301, 41))
-
-
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(10, 180, 391, 71))
         font = QtGui.QFont()
         font.setFamily("SF Pro Display")
-        font.setPointSize(9)
+        font.setPointSize(11)
         font.setBold(False)
         font.setWeight(50)
-        self.pushButton.setFont(font)
-        self.pushButton.setDefault(False)
-        self.pushButton.setFlat(False)
-        self.pushButton.setObjectName("pushButton")
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -58,8 +63,9 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label_2.setText(_translate("Dialog", "<html><head/><body><p align=\"center\">Success!</p></body></html>"))
-        self.pushButton.setText(_translate("Dialog", "Okay"))
+        self.okay_btn.setText(_translate("Dialog", "Okay"))
+        self.label_2.setText(_translate("Dialog", "<html><head/><body><p align=\"center\">Invalid ID</p></body></html>"))
+        self.label_4.setText(_translate("Dialog", "<html><head/><body><p align=\"center\">Please make sure to enter numbers and <br/>not any other character.</p></body></html>"))
 
 
 if __name__ == "__main__":
